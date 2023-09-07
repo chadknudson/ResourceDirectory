e86a4cd96ca08e7cc76ed12fdf3dedc6f4f6d377
Chad Knudson <cknudson@autodocs.com>
2023-09-07 11:49:50 -0500

    Add project files.

---
+++ ResourceDirectory/Options.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace ResourceDirectory
+{
+    public class Options
+    {
+        [Option('f', "file", Required = true, HelpText = "Full path and filename of the file containing the resources.")]
+        public string FilePath { get; set; }
+    }
+}
---
+++ ResourceDirectory/Program.cs
@@ -0,0 +1,34 @@
+﻿using CommandLine;
+using System.Reflection;
+
+namespace ResourceDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+            .WithParsed<Options>(o =>
+            {
+                if (File.Exists(o.FilePath))
+                {
+                    ResourceDirectory(o.FilePath);
+                }
+                else
+                {
+                    Console.WriteLine($"The source file {o.FilePath} was not located.");
+                }
+            });
+        }
+
+        public static void ResourceDirectory(string dllPath)
+        {           
+            var asm = Assembly.LoadFrom(dllPath);
+            string[] strings = asm.GetManifestResourceNames();
+            foreach (var s in strings)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
